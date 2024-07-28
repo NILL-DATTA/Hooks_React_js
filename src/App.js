46d14ceefb29029a689ea63ea7395b2adf4349f8
Sagnik Datta <sagnikdatta@Sagniks-MacBook-Air.local>
2024-07-28 14:35:34 +0530

    update

---
+++ src/App.js
@@ -0,0 +1,100 @@
+import logo from "./logo.svg";
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import { Component, Suspense, lazy } from "react";
+import Footer from "./Pages/Layout/Footer/Footer";
+import { toast } from "react-toastify";
+import BlogDetails from "./Pages/CMS/BlogDetails/BlogDetails";
+
+const Login = lazy(() => import("./Pages/Auth/Login/Login"));
+const Registration = lazy(() =>
+  import("./Pages/Auth/Registration/Registration")
+);
+const Header = lazy(() => import("./Pages/Layout/Header/Header"));
+const Update = lazy(() => import("./Pages/Auth/Update/Update"));
+const Home = lazy(() => import("./Pages/CMS/Home/Home"));
+const About = lazy(() => import("./Pages/CMS/About/About"));
+const Course = lazy(() => import("./Pages/CMS/Course/Course"));
+const Contact = lazy(() => import("./Pages/CMS/Contact/Contact"));
+
+const Blog = lazy(() => import("./Pages/CMS/Blog/Blog"));
+
+function Private({ children }) {
+  const token =
+    localStorage.getItem("token") || sessionStorage.getItem("token");
+  return token != null || token != undefined ? (
+    children
+  ) : (
+    <>
+      <Navigate to={"/"} />
+      {toast.error("login First")}
+    </>
+  );
+}
+
+const publicRoutesName = [
+  { path: "/login", Component: <Login /> },
+  {
+    path: "/registration",
+    Component: <Registration />,
+  },
+  {
+    path: "/updata",
+    Component: <Update />,
+  },
+  {
+    path: "/",
+    Component: <Home />,
+  },
+];
+const privateRoutesName = [
+  {
+    path: "/about",
+    Component: <About />,
+  },
+  {
+    path: "/Contact",
+    Component: <Contact />,
+  },
+  {
+    path: "/Course",
+    Component: <Course />,
+  },
+  {
+    path: "/Blog",
+    Component: <Blog />,
+  },
+  {
+    path:"/Blog/:id",
+    Component:<BlogDetails/>
+  }
+];
+
+function App() {
+  return (
+    <Suspense fallback={<h1>Loading......</h1>}>
+      <Router>
+        <Header />
+        <Routes>
+          {publicRoutesName.map((item) => (
+            <Route path={item.path} element={item.Component} />
+          ))}
+          {privateRoutesName.map((item) => (
+            <Route
+              path={item.path}
+              element={<Private>{item.Component}</Private>}
+            />
+          ))}
+        </Routes>
+        <Footer />
+      </Router>
+    </Suspense>
+  );
+}
+
+export default App;
